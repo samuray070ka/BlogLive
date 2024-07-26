9471e448bab71814ce3d75687a93fcc29a9a4074
samuray070ka <markelofmuhammadsodiq@gmail.com>
2024-07-27 00:32:59 +0500

    new

--- src/router/ishga-router/Kompaniya.js
+++ src/router/ishga-router/Kompaniya.js
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 function Kompaniya() {
   return (
-    <div>Kompaniya</div>
+    <div>
+      <img className='unique_page_img' src='https://piamg.uz/uploads/news_inner/KK/KK/pE/mosaic-financial-vybrana-konsultantom-po-privatizatsii.jpg' alt="" />
+      <div className="container">
+        <Link className='link' to={'/'}>
+          <h3 className='logo'>BOSH SAHIFA</h3>
+        </Link>
+        <h3 className='kompaniya_h4'>KORPORATIV BOSHQARUV BO'YICHA HUJJATLAR</h3>
+        <h3 className='komp_h3'>Ishlab chiqarish jarayonlarining ko'p bosqichli auditi </h3>
+        <h3 className='komp_h3'>Bilimlarni tekshirish</h3>
+        </div>
+    </div>
   )
 }
 
