3b49c86e3e0016c32501f8a2f36f788c5f1b8917
anvarjon-programmer <mranvarjontojiboyev@gmail.com>
2024-07-29 09:55:03 +0500

    sdfghjk

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -77,7 +77,7 @@ function Footer() {
 
         
 
-<footer className="bg-[#198754] dark:bg-gray-900">
+<footer className="bg-[#198754] ">
     <div className="mx-auto w-full max-w-screen-xl">
       <div className="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
         <div>
